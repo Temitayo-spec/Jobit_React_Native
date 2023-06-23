9865b79d0a4f3aae777c63efc52c74c3de859862
olawanle temitayo <olawanletemitayo@gmail.com>
2023-06-23 03:42:40 +0100

    feat; done with the basic layout for the welcome screen

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,75 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
 
-import styles from './welcome.style'
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style';
+
+import { icons, SIZES } from '../../../constants';
+
+const jobTypes = ['Full Time', 'Part Time', 'Contractor'];
 
 const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState(jobTypes[0]);
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Itachi</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="What are you looking for?"
+            placeholderTextColor="#000"
+            value=""
+            onChange={() => {}}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity
+                style={styles.tab(activeJobType, item)}
+                onPress={() => {
+                  setActiveJobType(item);
+                  router.push(`/search/${item}`);
+                }}
+              >
+                <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+              </TouchableOpacity>
+            );
+          }}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
